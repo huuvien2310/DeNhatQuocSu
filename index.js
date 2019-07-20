6a90009c15253b7531166d159e6fe9eca2e8dd75
huuvien2310 <52847097+huuvien2310@users.noreply.github.com>
2019-07-20 11:50:20 +0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,28 @@
+const Discord = require('discord.js');
+const { token, api, port } = require('./config.json');
+
+const client = new Discord.Client();
+
+client.on('message', (message) => {
+  client.user.setActivity("ANTV", {type: "WATCHING"})
+  
+  if (message.author.bot) return;
+
+  let blacklist = ['địt', 'đụ', 'đĩ', 'đỉ', 'lồn', 'loz', 'đụ má', 'đĩ mẹ','đụ mẹ', 'cặc', 'bot ngu'];
+
+  let foundInText = false;
+  for (var i in blacklist){
+    if (message.content.toLowerCase().includes(blacklist[i].toLowerCase())) foundInText = true;
+  }
+
+  if (foundInText){
+    message.react("🔫")
+    message.channel.send(`${message.author}, Đụ má mày chửi thề con cặc nói chuyện vô văn hóa! tao :gun: mày đó!`)
+  }
+});
+
+client.on('ready', () => {
+  console.log('Bot is now connected');
+});
+
+client.login(token);
