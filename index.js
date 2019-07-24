a9dad237092c1155f27e3bbda6d804181a5bb3bc
huuvien2310 <52847097+huuvien2310@users.noreply.github.com>
2019-07-24 08:22:33 +0700

    Update index.js

--- index.js
+++ index.js
@@ -4,22 +4,20 @@ const { token, api, port } = require('./config.json');
 const client = new Discord.Client();
 
 client.on('message', (message) => {
-  client.user.setActivity("ANTV", {type: "WATCHING"})
+  client.user.setActivity("Youtube", {type: "WATCHING"})
   
   if (message.author.bot) return;
 
-  let blacklist = ['địt', 'đụ', 'đĩ', 'đỉ', 'lồn', 'loz', 'đụ má', 'đĩ mẹ','đụ mẹ', 'cặc', 'bot ngu'];
+  let blacklist = ['địt', 'đụ', 'đĩ', 'đỉ', 'lồn', 'loz', 'đụ má', 'đĩ mẹ','đụ mẹ', 'cặc'];
 
   let foundInText = false;
   for (var i in blacklist){
     if (message.content.toLowerCase().includes(blacklist[i].toLowerCase())) foundInText = true;
   }
 
-  let customEmoji = message.guild.emojis.get("602504328070168604")
   if (foundInText){
     message.react("🔫")
-    message.react(customEmoji)
-    message.channel.send(`${message.author}, Đụ má mày chửi thề con cặc nói chuyện vô văn hóa! tao :gun: mày đó!`)
+    message.channel.send(`${message.author}, Đụ má mày chửi thề con cặc nói chuyện vô văn hóa! Tao :gun: mày đó!`)
   }
 });
 
