4c2f857e9ed0ce965da0561da17d70f35d9be33b
Vien Hua <vhua@worcester.edu>
2021-04-05 20:57:55 -0400

    Pin pin pin

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const { api, port } = require('./config.json');
 require("dotenv").config();
 
 const client = new Discord.Client();
-const prefix = '!';
+const prefix = '_';
 
 client.on('message', (message) => {
   client.user.setActivity("Youtube", {type: "WATCHING"})
@@ -13,29 +13,22 @@ client.on('message', (message) => {
   let blacklist = ['địt', 'đụ', 'đĩ', 'đỉ', 'lồn', 'loz', 'đụ má', 'đĩ mẹ','đụ mẹ', 'cặc'];
 
   let foundInText = false;
+
   for (var i in blacklist){
     if (message.content.toLowerCase().includes(blacklist[i].toLowerCase())) foundInText = true;
   }
 
   if (foundInText){
     message.react("🔫")
-    let customEmoji = message.guild.emojis.get("631511891440828431")
     message.channel.send(`${message.author}, Đụ má mày chửi thề con cặc nói chuyện vô văn hóa! Tao :gun: mày đó!`)
-    message.react(customEmoji)
   }
 
   let command = message.content.split(' ')[0].slice(1);
-  let args = message.content.replace('!' + command, '').trim();
+  let args = message.content.replace('_' + command, '').trim();
 
-  if(!command) return;
-
-  switch(command){
-    case "pin":
-      client.channels.get(`828441457093771284`).send(`📌${args}`);
-      //message.channel.send(`📌${args}`);
-      break;
+  if(command == "pin" && !foundInText){
+    client.channels.get(`828795393365770270`).send(`📌${args}`);
   }
-
 });
 
 client.on('ready', () => {
